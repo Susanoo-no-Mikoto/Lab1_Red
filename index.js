5545331c1d107759b19c40d1af6c54feb5646f4d
Артём Вячеславович Фадеев <19300707@live.inueco.ru>
2021-02-19 10:41:37 +0500

    qr commit

--- index.js
+++ index.js
@@ -9,28 +9,31 @@ function fsd(){
     arg.question('Укажите проценты? ', (arg2) => {
         
         arg2 = arg2.split(' ');
-        let summ = 0;
+        let summ = arg1;
         let res;
         let arr = [];
         for (let i = 0; i < arg2.length; i++){
             let a;
             a = arg2[i] / 100;
-            a = arg1 * a;
+            a = summ * a;
             arr.push(a);
             res = arg1 + a; 
             console.log(arg2[i]+'%');
             console.log(res);
+            let ss = 0;
+            for (let j = 0; j < arr.length; j++){
+                if ( arr[j-1] != isNaN){
+                    ss = arr[j] + ss;
+                } else{ 
+                    ss = arr[j];
+                }    
+            }
+            summ = ss + arg1;
         }
         //console.log(arr);
-        for (let i = 0; i < arr.length; i++){
-            if ( arr[i-1] != isNaN){
-                summ = arr[i] + summ;
-            } else{ 
-                summ = arr[i];
-            }    
-        }
-        summ = summ + arg1;
-            console.log(summ);
+        
+        
+        console.log(summ);
         arg.close();
     });
 }
